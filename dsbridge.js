d5d5bc45f7a95df73d62050ee31e9b830c004c2b
leo.shi <554601845@qq.com>
2024-10-11 16:40:25 +0800

    first commit

---
+++ dsbridge.js
@@ -0,0 +1,12 @@
+var dsBridge = require('dsbridge')
+export default {
+    callmethod (name, data, callback) {
+        callback(dsBridge.call(name, data, callback))
+    },
+    registermethod (tag, callback) {
+        dsBridge.register(tag, callback)
+    },
+    registerAsynmethod (tag, callback) {
+        dsBridge.registerAsyn(tag, callback)
+    }
+}
